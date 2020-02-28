f005e8966d6101aeb9445484133b369573157b9c
rachaelmattison <54870880+rachaelmattison@users.noreply.github.com>
2020-02-28 14:00:15 -0500

    added js practice homework

---
+++ js-hw01/jshw01.js
@@ -0,0 +1,27 @@
+function onScroll(color) {
+	document.getElementById('background').style.backgroundColor=color;
+
+}
+function changebackgroundColor(color) {
+	document.getElementById('whatcolor').style.backgroundColor=color;
+	document.getElementById('clickme').style.backgroundColor=color;
+}
+
+function changeHeading(color) {
+	document.getElementById('whatcolor').style.backgroundColor=color;
+}
+
+function changeSubHeading(color) {
+	document.getElementById('tensions').style.backgroundColor=color;
+	document.getElementById('tensions').innerHTML= " 0's & 1's SADIE PLANT"
+}
+
+
+function changeWords(words) {
+	document.getElementById('words').innerHTML=words
+}
+
+	// document.getElementById('words').onkeypress = function {
+	// 	changeWords('RACHAEL WAS HERE')}; 
+
+
