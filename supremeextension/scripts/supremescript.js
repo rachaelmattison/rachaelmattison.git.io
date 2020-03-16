74541f9fd01c031b5b026ff58fecab9e125cc33b
rachaelmattison <54870880+rachaelmattison@users.noreply.github.com>
2020-03-16 17:32:10 -0400

    added parasite documentation and there's extension on it

---
+++ supremeextension/scripts/supremescript.js
@@ -0,0 +1,17 @@
+$('header').remove();
+
+// var imgs = document.querySelectorAll("img");
+
+// for (var i = 0, 1=imgs.length; i < 1; i++) {
+// 	imgs[i].src="https://d2w9rnfcy7mm78.cloudfront.net/4877544/large_d92a3d921d60da77aad57e655fa79eb9.jpg?1566491326?bc=1";
+// }
+
+
+// $('img').attr({'src': 'https://www.youtube.com/watch?v=CXaAgSMhnfs'});
+
+
+
+document.querySelector('span').body.style.backgroundColor('purple');
+
+
+span.innerHTML = "this"
